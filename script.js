d1f8df4ce94caf3ec6f752c16fcc7eebbf71f08d
WebDevPie <nazarmarchyshyn@gmail.com>
2023-02-16 13:47:49 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#typed', {
+    strings: ['Hello there!', 'How are you?'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    // fadeOut: true,
+    loop: true,
+    loopCount: 2,
+    // showCursor: false
+    // cursorChar: '...'
+
+});
